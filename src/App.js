3bb3fb1052779baabd1076c60b33703101b36620
evgenik <kozyr.e@gmx.de>
2019-08-20 09:28:53 +0200

    dependencies dabei

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Kräuter sind geil
         </p>
@@ -16,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Kräuter
+          Learn React
         </a>
       </header>
     </div>
