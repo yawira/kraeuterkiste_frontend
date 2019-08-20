a785b7ed03b06c86e5f7601e4d8cd02dbd3ab0c6
Lorenz Josten <lorenz.josten@gmail.com>
2019-08-20 09:29:42 +0200

    try implement amcharts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MoistureChart from './MoistureChart'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kräuter sind geil
-        </p>
-        <a
-          className="App-link"
-          href="https://www.drugcom.de/drogenlexikon/buchstabe-c/cannabis/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Kräuter
-        </a>
-      </header>
+      <MoistureChart />
     </div>
   );
 }
