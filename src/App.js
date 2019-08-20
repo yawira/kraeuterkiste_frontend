441dc4995e95ea29d08f05cc904e58d4cdb35d5c
evgenik <kozyr.e@gmx.de>
2019-08-20 11:16:18 +0200

    first 2 ColumnCharts included

--- src/App.js
+++ src/App.js
@@ -1,13 +1,129 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import MoistureChart from "./MoistureChart";
+import ColumnChart from './ColumnChart';
 
-function App() {
-  return (
-    <div className="App">
-        <MoistureChart />
-    </div>
-  );
+export default class App extends Component {
+  
+
+ generateMoistureData = () => {
+     return [
+      {
+        country: "USA",
+        visits: 23725
+      },
+      {
+        country: "China",
+        visits: 1882
+      },
+      {
+        country: "Japan",
+        visits: 1809
+      },
+      {
+        country: "Germany",
+        visits: 1322
+      },
+      {
+        country: "UK",
+        visits: 1122
+      },
+      {
+        country: "France",
+        visits: 1114
+      },
+      {
+        country: "India",
+        visits: 984
+      },
+      {
+        country: "Spain",
+        visits: 711
+      },
+      {
+        country: "Netherlands",
+        visits: 665
+      },
+      {
+        country: "Russia",
+        visits: 580
+      },
+      {
+        country: "South Korea",
+        visits: 443
+      },
+      {
+        country: "Canada",
+        visits: 441
+      }
+    ];
+
+  } 
+
+  generateWaterData = () => {
+    return [
+     {
+       country: "USA",
+       visits: 23725
+     },
+     {
+       country: "China",
+       visits: 1882
+     },
+     {
+       country: "Japan",
+       visits: 1809
+     },
+     {
+       country: "Germany",
+       visits: 1322
+     },
+     {
+       country: "UK",
+       visits: 1122
+     },
+     {
+       country: "France",
+       visits: 1114
+     },
+     {
+       country: "India",
+       visits: 984
+     },
+     {
+       country: "Spain",
+       visits: 711
+     },
+     {
+       country: "Netherlands",
+       visits: 665
+     },
+     {
+       country: "Russia",
+       visits: 580
+     },
+     {
+       country: "South Korea",
+       visits: 443
+     },
+     {
+       country: "Canada",
+       visits: 441
+     }
+   ];
+
+ } 
+  
+render(){
+  const moistureData = this.generateMoistureData()
+  const waterData = this.generateWaterData()
+    return (
+      <div className="App">
+          {/* everything passed inside the curly braces is passed to the "props" */}
+          <ColumnChart div={"moistureChart"} data={moistureData}/>
+          <ColumnChart div={"waterChart"} data={waterData}/>
+      </div>
+    );
+  }
 }
 
-export default App;
---
+++ src/ColumnChart.js
@@ -0,0 +1,76 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated"
+import React, {Component} from 'react'
+
+export default class ColumnChart extends Component {
+
+  //componentDidMount is used to create the chart
+  componentDidMount() {
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+      /* Chart code */
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+    chart.data = this.props.data;
+
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.dataFields.category = "country";
+    categoryAxis.renderer.minGridDistance = 40;
+    categoryAxis.fontSize = 11;
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.min = 0;
+    valueAxis.max = 24000;
+    valueAxis.strictMinMax = true;
+    valueAxis.renderer.minGridDistance = 30;
+    // axis break
+    let axisBreak = valueAxis.axisBreaks.create();
+    axisBreak.startValue = 2100;
+    axisBreak.endValue = 22900;
+    axisBreak.breakSize = 0.005;
+
+    // make break expand on hover
+    let hoverState = axisBreak.states.create("hover");
+    hoverState.properties.breakSize = 1;
+    hoverState.properties.opacity = 0.1;
+    hoverState.transitionDuration = 1500;
+
+    axisBreak.defaultState.transitionDuration = 1000;
+    
+
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.categoryX = "country";
+    series.dataFields.valueY = "visits";
+    series.columns.template.tooltipText = "{valueY.value}";
+    series.columns.template.tooltipY = 0;
+    series.columns.template.strokeOpacity = 0;
+
+    // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+    series.columns.template.adapter.add("fill", function(fill, target) {
+      return chart.colors.getIndex(target.dataItem.index);
+    });
+
+    this.chart = chart;
+  }
+
+  //componentWillUnmount is used to cleanup the chart when it's done being used.
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div id={this.props.div} style={{ width: "100%", height: "500px" }}></div>
+    );
+  }
+}
