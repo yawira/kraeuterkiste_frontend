24462a144351b4ecdd94f5d740a8870fe7cb1af0
evgenik <kozyr.e@gmx.de>
2019-08-21 15:58:31 +0200

    successfully merged master into ConfigViewNew

--- src/Configuration.js
+++ src/Configuration.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Configuration extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h2>GOT QUESTIONS?</h2>
         <p>The easiest thing to do is post on
         our <a href="https://www.drugcom.de/drogenlexikon/buchstabe-c/cannabis/">forums</a>.
--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import ColumnChart from "./ColumnChart";
 import LineChart from "./LineChart";
 import "./charts.css";
 import Photo from "./Photo";
+import { Button } from "react-bootstrap";
 
 export default class Home extends Component {
   constructor(props) {
