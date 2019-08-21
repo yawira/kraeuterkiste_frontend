7285b63db05dbb90b5303d271f03181bdaa55369
evgenik <kozyr.e@gmx.de>
2019-08-21 15:15:28 +0200

    SPA created

---
+++ src/Configuration.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+export default class Configuration extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is post on
+        our <a href="https://www.drugcom.de/drogenlexikon/buchstabe-c/cannabis/">forums</a>.
+        </p>
+      </div>
+    );
+  }
+};
