6e414ca0d472be9e8b71fd0cf094c796fe5c252d
Lorenz Josten <lorenz.josten@gmail.com>
2019-08-28 17:52:19 +0200

    fix App is functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import "./charts/charts.css"
 import MyNavbar from "./Logic/Navbar";
 
 
-export default class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div>
-                    <MyNavbar/>
-                </div>
-            </div>         
-        );
-    }
+export default function App() {
+    return (
+        <div className="App">
+            <div>
+                <MyNavbar/>
+            </div>
+        </div>
+    );
 }
